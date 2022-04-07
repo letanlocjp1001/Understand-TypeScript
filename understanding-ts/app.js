7732fd8c484d554fe666544f04ac39c86e5e1f2f
Le Tan Loc <letanlocjp1001@gmail.com>
2022-04-07 20:03:47 +0900

    types

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,1 +1,14 @@
-console.log('Hello TypeScript....');
+function combine(n1, n2) {
+    var result;
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        result = n1 + n2;
+    }
+    else {
+        result = n1.toString() + n2.toString();
+    }
+    return result;
+}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('MAx', 'Anna');
+console.log(combinedNames);
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello TypeScript....')
+function combine(n1: number | string, n2: number | string) {
+  let result
+  if (typeof n1 === 'number' && typeof n2 === 'number') {
+    result = n1 + n2
+  } else {
+    result = n1.toString() + n2.toString()
+  }
+  return result
+}
+
+const combinedAges = combine(30, 26)
+console.log(combinedAges)
+
+const combinedNames = combine('MAx', 'Anna')
+console.log(combinedNames)
